a78b0f6ee3955dfa68f81877909ca14bb8da1983
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-10-06 00:00:36 +0200

    Fully functioning switch to dark and light themes

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,20 @@ let colorState = true;
 
 button.addEventListener("click", () => {
   if (colorState === true) {
+    // Page color
     document.body.style.backgroundColor = "rgb(0,0,0)";
     document.body.style.color = "rgb(245,245,245)";
+
+    // Button text
+    button.innerText = "Light Theme";
     colorState = false;
   } else {
+    // Page color
     document.body.style.backgroundColor = "rgb(255,255,255)";
     document.body.style.color = "rgb(0,0,0)";
+
+    // Button text
+    button.innerText = "Dark Theme";
     colorState = true;
   }
 });
