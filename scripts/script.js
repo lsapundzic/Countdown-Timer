b85457e54f394bec0ff163df897a1af6781c88f0
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-10-05 23:57:48 +0200

    Switching between states

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
 const button = document.querySelector("button");
+let colorState = true;
 
-button.addEventListener("click", function () {
-  document.body.style.backgroundColor = "rgb(0,0,0)";
-  document.body.style.color = "rgb(245,245,245)";
+button.addEventListener("click", () => {
+  if (colorState === true) {
+    document.body.style.backgroundColor = "rgb(0,0,0)";
+    document.body.style.color = "rgb(245,245,245)";
+    colorState = false;
+  } else {
+    document.body.style.backgroundColor = "rgb(255,255,255)";
+    document.body.style.color = "rgb(0,0,0)";
+    colorState = true;
+  }
 });
