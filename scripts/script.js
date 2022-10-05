e94d709c630bbcff6bc7874966fc27ca6e3fdb7f
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2022-10-06 00:23:16 +0200

    standardized script

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,27 @@
 const button = document.querySelector("button");
+
+// Default states
 let colorState = true;
 button.innerText = "Dark Theme";
+
+// Colors
+let black = "rgb(0,0,0)";
+let whitesmoke = "rgb(245,245,245)";
+let white = "rgb(255,255,255)";
 
 button.addEventListener("click", () => {
   if (colorState === true) {
     // Page color
-    document.body.style.backgroundColor = "rgb(0,0,0)";
-    document.body.style.color = "rgb(245,245,245)";
+    document.body.style.backgroundColor = black;
+    document.body.style.color = whitesmoke;
 
     // Button text
     button.innerText = "Light Theme";
     colorState = false;
   } else {
     // Page color
-    document.body.style.backgroundColor = "rgb(255,255,255)";
-    document.body.style.color = "rgb(0,0,0)";
+    document.body.style.backgroundColor = white;
+    document.body.style.color = black;
 
     // Button text
     button.innerText = "Dark Theme";
